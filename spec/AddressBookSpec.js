ce5186af46455fd9d2ff012dc4dc35ff9af86d38
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-03-07 09:32:37 +0200

    Lesson 2 Jasmine - Mike's Solution

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,7 +1,7 @@
 describe('Address Book', function() {
     it('should be able to add a contact', function() {
         var addressBook = new AddressBook();
-        thisContact = new Contact();
+            thisContact = new Contact();
 
         addressBook.addContact(thisContact);
 
