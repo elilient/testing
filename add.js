ed92f741aad84586297dabac74cd9970e833bb08
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-03-07 09:16:38 +0200

    Lesson 2 Jasmine - Writing a Test

--- add.js
+++ add.js
@@ -1,13 +1,3 @@
-function add(x, y) {
-    var result;
-    if ((typeof x && typeof y) !== 'number') {
-        throw new Error('Params must be a number.');
-    }
 
-    return x + y;
-    if (parseInt(result) !== result) {
-        result = parseFloat(result.toFixed(1));
-    }
-}
-
-expect(add(2, 3)).toBe(5)
+expect(add(0.1, 0.2)).toBe(0.3);
+expect(add(0.1, 0.2)).not.toBe(0.1);
